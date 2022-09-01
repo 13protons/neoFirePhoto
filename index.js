21413788dc8b00f6f3ee8a2af7281f939cc016eb
Alan Languirand <alan@13protons.com>
2022-09-01 00:29:35 -0400

    build: prefer indexed color with image pixel data stored in PROGMEM

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-var Jimp = require('jimp');
+const Jimp = require('jimp');
+const _ = require('lodash');
+
 
 const Rotate = {
   '_0': 0,      // no adjustment
@@ -7,7 +9,7 @@ const Rotate = {
   '_270': 270   // rotate 270 degrees clockwise
 }
 
-const subject = './assets/stripes.png'; // file to read. Make sure the path is good
+const subject = './assets/sunset.png'; // file to read. Make sure the path is good
 const settings = {
   pixels: 20,       // how many neopixels in the group?
   fps: 5,           // how many frames per second?
@@ -21,6 +23,19 @@ function getMatte(width, height) {
       resolve(image);
     });
   });
+}
+
+function strigifyArrayForCcode(input) {
+  var expanded = JSON.stringify(input);
+  expanded = expanded.replace(/\[/g, '{');
+  expanded = expanded.replace(/\]/g, '}');
+  expanded = expanded.replace(/\"/g, '');
+  return expanded;
+}
+
+function expand(data, template) {
+  const expanded = strigifyArrayForCcode(data);
+  console.log(template({data: expanded}));
 }
 
 Jimp.read(subject)
@@ -39,49 +54,62 @@ Jimp.read(subject)
         });
 
         flat.write('newFirePhoto_preview.jpg'); // save
-
-
-        var output_head = `uint8_t sequence[${flat.bitmap.height}][${flat.bitmap.width * 3}]`
-        var rows = [];
-
-        for(var i = 0; i < flat.bitmap.height; i++) {
-          // scan 1 row at a time
-          rows.push([]);
-          img.scan(0, i, flat.bitmap.width, 1, function(x, y, idx) {
-            var red = flat.bitmap.data[idx + 0];
-            var green = flat.bitmap.data[idx + 1];
-            var blue = flat.bitmap.data[idx + 2];
-
-            const hexVal = [
-              red.toString(16).padStart(2, '0'),
-              green.toString(16).padStart(2, '0'),
-              blue.toString(16).padStart(2, '0')
-            ].join('').toUpperCase();
-
-            rows[i].push(red)
-            rows[i].push(green)
-            rows[i].push(blue)
-            // rows[i].push([red, green, blue]);
-          });
-
-          
-        }
-
-        var expanded = JSON.stringify(rows);
-        expanded = expanded.replace(/\[/g, '{');
-        expanded = expanded.replace(/\]/g, '}');
-        expanded = expanded.replace(/\"/g, '');
-
+        
         console.log(' ');
         console.log(' ');
         console.log(' ');
         console.log(`//========${subject}======== - generated ${Date.now()}`);
         console.log(' ');
-        console.log(`uint8_t seq_width = ${flat.bitmap.width}; // width of image sequence data`);
-        console.log(`uint8_t seq_height = ${flat.bitmap.height}; // width of image sequence data`);
-        console.log(`uint8_t seq_delay = ${Math.round(1000/settings.fps)}; // delay in ms`);
+        console.log(`const int seq_width = ${flat.bitmap.width}; // width of image sequence data`);
+        console.log(`const int seq_height = ${flat.bitmap.height}; // width of image sequence data`);
+        console.log(`const int seq_delay = ${Math.round(1000/settings.fps)}; // delay in ms`);
         console.log(' ');
-        console.log(output_head + '=' + expanded + ';');
+
+        
+        const rowTemplate = _.template(`const uint8_t seq[${flat.bitmap.height}][${flat.bitmap.width}] PROGMEM = <%= data %>;`);
+        
+
+        const palletteTemplate = _.template(`const int pallette[] = <%= data %>;`)
+        
+        const pallette = [];
+        const rows = [];
+
+        for(var i = 0; i < flat.bitmap.height; i++) {
+          // scan 1 row at a time
+          const row = [];
+          img.scan(0, i, flat.bitmap.width, 1, function(x, y, idx) {
+            
+            var red = flat.bitmap.data[idx + 0];
+            var green = flat.bitmap.data[idx + 1];
+            var blue = flat.bitmap.data[idx + 2];
+
+            const hexVal = '0x' + [
+              red.toString(16).padStart(2, '0'),
+              green.toString(16).padStart(2, '0'),
+              blue.toString(16).padStart(2, '0')
+            ].join('').toUpperCase();
+
+            var found = pallette.findIndex((item)=>item == hexVal);
+            
+            if (found > -1) {
+              // this is the index of our indexed color
+              row.push(found);
+            } else {
+              row.push(pallette.length);
+              pallette.push(hexVal);
+            }
+            
+          });
+         
+          rows.push(row);
+        }
+
+        
+          expand(rows, rowTemplate)
+        
+          
+        expand(pallette, palletteTemplate);       
+        
       });
       
     
